8190f1db88c4d700edce094d79c81d8a487b85fd
User <mr.winterwolf@gmail.com>
2020-08-19 23:25:57 +0300

    changed progect Function and done homework 'Function'

--- KotlinBasic/02_Functions/Function/app/src/main/java/com/skillbox/function/Function.kt
+++ KotlinBasic/02_Functions/Function/app/src/main/java/com/skillbox/function/Function.kt
@@ -1,9 +1,30 @@
 package com.skillbox.function
 
+import kotlin.math.sqrt
+
 fun main() {
-    print(solveEquation(2, 3, 4))
+    print(solveEquation(10.0, 30.0, 11.0))
 }
 
-fun solveEquation(a: Int, b: Int, c: Int): Int {
-    return (b * b) - 4 * a * c
+fun solveEquation(a: Double, b: Double, c: Double): Any {
+    val isEqualityABC: Boolean = (a == b || a == c) || (b == a || b == c) || (c == a || c == b)
+    val discriminant: Double = (b * b) - 4 * a * c
+    val sqrtDsrmt: Double = sqrt(discriminant)
+    val x1: Double = (-b + sqrtDsrmt) / (2 * a)
+    val x2: Double = (-b - sqrtDsrmt) / (2 * a)
+    val x3: Double = -b / 2 * a
+    val error: String = "При решении квардратных уравнений: \n" +
+            "1. - \"а\" не должно быть равным \"0\"\n" +
+            "2. - числа \"а\", \"b\"  и \"с\" не должны быть равными друг другу"
+    val noRoots: String = "Уравнение не имеет корней"
+
+    if (a == 0.0 || isEqualityABC) {
+        return error
+    }
+    else if (discriminant > 0) {return x1 * x2}
+    else if (discriminant == 0.0) {return x3}
+    else if (discriminant < 0){return noRoots}
+return 0
 }
+
+
