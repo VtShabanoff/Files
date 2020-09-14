503ac8be9b5f1a14c7a5082163a2fbd54c8c25a6
shabanov_vitalii <vitalyshabanoff@yandex.ru>
2020-09-14 12:32:13 +0300

    done homework loops, recursion and nullable types

---
+++ KotlinBasic/03_Loops_Recursion_NullableTypes/app/src/main/java/com/example/loops_recursion_nullabletypes/LRN.kt
@@ -0,0 +1,53 @@
+package com.example.loops_recursion_nullabletypes
+
+fun main() {
+    val a = readLine()?.toIntOrNull() ?: return
+    val b = readLine()?.toIntOrNull() ?: return
+    //inputInt()
+    //printPositiveNumbersAndSumNumbers()
+   // println(gCD(a, b))
+
+}
+
+fun inputInt() {
+    println("Введите число -")
+    while (true){
+        val lineInt = readLine()?.toIntOrNull()
+        if (lineInt == null) continue else break
+    }
+
+}
+fun printPositiveNumbersAndSumNumbers(){
+    var count = 0;
+    var sum = 0
+    var countPositiveNumbers = 0
+
+    while (count < 5){
+        println("Введите числа положительные или отрицательные типа Int")
+        var lineNumberInt = readLine()?.toIntOrNull()
+
+        if (lineNumberInt != null &&  lineNumberInt >= 0) {
+           println(" число положительное")
+            countPositiveNumbers++
+        } else if (lineNumberInt != null && lineNumberInt < 0)println(" число отрицательное")
+
+        else println("Вы ввели не число")
+
+        count++
+
+        if (lineNumberInt != null) {
+            sum += lineNumberInt
+        }
+        if (count == 5) break
+    }
+
+    println("количество введенных положительных чисел = $countPositiveNumbers")
+    println("сумма всех введенных чисел  = $sum")
+}
+fun gCD(a: Int, b: Int): Int? {
+
+    return if (b == 0){
+       a
+    } else gCD(b, a % b)
+
+}
