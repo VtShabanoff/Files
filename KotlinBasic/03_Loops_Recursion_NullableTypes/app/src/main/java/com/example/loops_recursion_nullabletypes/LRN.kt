2d410ba36b55a78a01fb4dc193a72e28f45a540a
shabanov_vitalii <vitalyshabanoff@yandex.ru>
2020-09-21 11:49:24 +0300

    chanched home work

--- KotlinBasic/03_Loops_Recursion_NullableTypes/app/src/main/java/com/example/loops_recursion_nullabletypes/LRN.kt
+++ KotlinBasic/03_Loops_Recursion_NullableTypes/app/src/main/java/com/example/loops_recursion_nullabletypes/LRN.kt
@@ -1,20 +1,22 @@
 package com.example.loops_recursion_nullabletypes
 
 fun main() {
+
+    println(inputInt())
+    printPositiveNumbersAndSumNumbers()
+    println("Введите два числа для функции gCD - вычисления НОД =>")
     val a = readLine()?.toIntOrNull() ?: return
     val b = readLine()?.toIntOrNull() ?: return
-    //inputInt()
-    //printPositiveNumbersAndSumNumbers()
-   // println(gCD(a, b))
+    println(gCD(a, b))
 
 }
 
-fun inputInt() {
-    println("Введите число -")
+fun inputInt():Int {
+    println("Введите число для функции inputInt =>")
     while (true){
-        val lineInt = readLine()?.toIntOrNull()
-        if (lineInt == null) continue else break
+        return readLine()?.toIntOrNull() ?: continue
     }
+
 
 }
 fun printPositiveNumbersAndSumNumbers(){
@@ -22,32 +24,40 @@ fun printPositiveNumbersAndSumNumbers(){
     var sum = 0
     var countPositiveNumbers = 0
 
-    while (count < 5){
-        println("Введите числа положительные или отрицательные типа Int")
-        var lineNumberInt = readLine()?.toIntOrNull()
+    println("Функция printPositiveNumbersAndSumNumbers \nВведите число для итерации =>")
+    val a = readLine()?.toIntOrNull() ?: return
+    while (count < a){
+        println("Введите числа положительные или отрицательные типа Int =>")
+        val b = readLine()?.toIntOrNull()
 
-        if (lineNumberInt != null &&  lineNumberInt >= 0) {
-           println(" число положительное")
-            countPositiveNumbers++
-        } else if (lineNumberInt != null && lineNumberInt < 0)println(" число отрицательное")
-
-        else println("Вы ввели не число")
+        if (b != null) {
+            when {
+                b >= 0 -> {
+                    println(" число положительное")
+                    countPositiveNumbers++
+                }
+                b < 0 -> {
+                    println(" число отрицательное")
+                }
+                else -> {println("Вы ввели не число")}
+            }
+        }
 
         count++
 
-        if (lineNumberInt != null) {
-            sum += lineNumberInt
+        if (b != null) {
+            sum += b
         }
-        if (count == 5) break
     }
 
     println("количество введенных положительных чисел = $countPositiveNumbers")
     println("сумма всех введенных чисел  = $sum")
 }
-tailrec fun gCD(a: Int, b: Int): Int? {
-
+tailrec fun gCD(a: Int, b: Int): Int {
+    val sum = a + b
+    println("сумма введенных чисел = $sum")
     return if (b == 0){
        a
     } else gCD(b, a % b)
-
+    
 }
