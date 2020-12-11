9b7acf9e39ff5bb91c56e27c49efd46cba86e89a
VtShabanoff <vitalyshabanoff@yandex.ru>
2020-12-11 10:54:39 +0300

    added home work points 1 - 4

---
+++ Extensions_Objects_Enums/app/src/main/java/enums/Ammo.kt
@@ -0,0 +1,35 @@
+package enums
+
+import kotlin.random.Random
+
+enum class Ammo(
+    val strike: Int,
+    val criticalStrikeChance: Int,
+    val criticalStrikeRatio: Int
+) {
+    PISTOL(
+        strike = 2,
+        criticalStrikeChance = 10,
+        criticalStrikeRatio = 4
+    ),
+    RIFLE(
+        strike = 8,
+        criticalStrikeChance = 25,
+        criticalStrikeRatio = 18
+    ),
+    SHOTGUN(
+        strike = 20,
+        criticalStrikeChance = 35,
+        criticalStrikeRatio = 45
+    );
+
+    fun getDamage(): Int {
+        return if (criticalStrikeChance.isChance()){
+            strike * criticalStrikeRatio
+        } else {
+            strike
+        }
+    }
+
+}
+fun Int.isChance(): Boolean = Random.nextInt(100) < this
---
+++ Extensions_Objects_Enums/app/src/main/java/other_classes/AbstractWeapon.kt
@@ -0,0 +1,34 @@
+package other_classes
+
+import enums.Ammo
+
+abstract class AbstractWeapon(
+        private val maxNumbersOfRounds: Int,
+        private val fireType: FireType,
+) {
+
+    var listRounds: List<Ammo> = listOf()
+    val isAvailabilityOfRounds: Boolean
+        get() = listRounds.isNotEmpty()
+
+    abstract fun createRound(): Ammo
+
+    fun reloadingWeapons(){
+        val newList = mutableListOf<Ammo>()
+        for (i in 0..maxNumbersOfRounds){
+            newList.add(createRound())
+        }
+        listRounds = newList
+    }
+    fun getRoundsForShot(): List<Ammo> {
+        val result: List<Ammo>
+        if (fireType is FireType.BurstShoot) {
+           result = listRounds.take(fireType.burstSize)
+            listRounds = listRounds.drop(fireType.burstSize)
+        } else {
+            result = listRounds.take(1)
+            listRounds = listRounds.drop(1)
+        }
+        return result
+    }
+}
