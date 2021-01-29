c2dc5e9a52ccbd2d2c4c4cf5cf5fa86b441d3ccd
VtShabanoff <vitalyshabanoff@yandex.ru>
2021-01-29 22:15:37 +0300

    added 1 point of homework 8.4

---
+++ KotlinHighOrderFunctionAndException/01_Generics/app/src/main/java/com/example/generics/Main.kt
@@ -0,0 +1,11 @@
+package com.example.generics
+
+fun main(){
+    val list = listOf(20, 30, 77)
+
+    filterEven(list).forEach { println(it) }
+}
+
+fun <T: Number> filterEven(list: List<T>): List<T>{
+    return list.filter { it.toDouble() % 2.0 == 0.0 }
+}
