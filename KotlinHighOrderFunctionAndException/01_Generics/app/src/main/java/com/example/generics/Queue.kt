e1eb4745e5907ef2c5290ee173048764a6950d9d
VtShabanoff <vitalyshabanoff@yandex.ru>
2021-02-18 18:01:48 +0300

    added homework 8.7

--- KotlinHighOrderFunctionAndException/01_Generics/app/src/main/java/com/example/generics/Queue.kt
+++ KotlinHighOrderFunctionAndException/01_Generics/app/src/main/java/com/example/generics/Queue.kt
@@ -1,5 +1,8 @@
 package com.example.generics
 
+import java.util.function.BiPredicate
+
+
 class Queue<T> {
     var items: MutableList<T> = mutableListOf()
     fun enqueue(item: T){
@@ -9,5 +12,15 @@ class Queue<T> {
     fun dequeue(): T?{
         return if (items.isNotEmpty()) items[0] else null
 
+    }
+
+    fun filterQueue(predicate: (T) -> Boolean ): Queue<T> {
+        val newQueue = Queue<T>()
+            for (item in this.items){
+                if (predicate(item)){
+                newQueue.items.add(item)
+            }
+        }
+        return newQueue
     }
 }
