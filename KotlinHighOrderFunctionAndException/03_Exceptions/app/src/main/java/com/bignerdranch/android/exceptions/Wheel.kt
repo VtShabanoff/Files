99c56fd7f516aaa11519725d3ec5bbe38457e80d
VtShabanoff <vitalyshabanoff@yandex.ru>
2021-02-24 16:19:22 +0300

    added homework points 1-5

---
+++ KotlinHighOrderFunctionAndException/03_Exceptions/app/src/main/java/com/bignerdranch/android/exceptions/Wheel.kt
@@ -0,0 +1,24 @@
+package com.bignerdranch.android.exceptions
+
+class Wheel {
+    var pressure: Double = 0.0
+        private set
+
+    fun setPressure(value: Double) {
+        when  {
+            value > 10.0 || value < 0.0 -> IncorrectPressure()
+        }
+    }
+
+
+    class TooHighPressure: Exception(){
+
+    }
+    class TooLowPressure: Exception(){
+
+    }
+    class IncorrectPressure: Exception(){
+
+    }
+    
+}
