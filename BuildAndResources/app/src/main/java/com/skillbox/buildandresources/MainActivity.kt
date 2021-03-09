5044a822b4ef0068268ba48161b718e968b7578f
VtShabanoff <vitalyshabanoff@yandex.ru>
2021-03-09 21:54:41 +0300

    added point 4 of homework 9.13

--- BuildAndResources/app/src/main/java/com/skillbox/buildandresources/MainActivity.kt
+++ BuildAndResources/app/src/main/java/com/skillbox/buildandresources/MainActivity.kt
@@ -2,10 +2,22 @@ package com.skillbox.buildandresources
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+
+        textView.text = """"
+            Build type = ${BuildConfig.BUILD_TYPE}
+            Flavor = ${BuildConfig.FLAVOR}
+            Version code = ${BuildConfig.VERSION_CODE}
+            Version name = ${BuildConfig.VERSION_NAME}
+            Version name = ${BuildConfig.VERSION_NAME}
+            Application Id = ${BuildConfig.APPLICATION_ID}
+        """"
     }
 }
