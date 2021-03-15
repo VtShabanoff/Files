6b222e7b10508af0016b9b480508a144f50494fd
VtShabanoff <vitalyshabanoff@yandex.ru>
2021-03-15 18:44:41 +0300

    added homework 10.8

--- ViewAndLayout/app/src/main/java/com/skillbox/viewandlayout/MainActivity.kt
+++ ViewAndLayout/app/src/main/java/com/skillbox/viewandlayout/MainActivity.kt
@@ -2,6 +2,9 @@ package com.skillbox.viewandlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,15 +12,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        checkboxTermsOfUse.setOnCheckedChangeListener { compoundButton, isChecked ->
-
+        checkboxTermsOfUse.setOnCheckedChangeListener { _, _ ->
             logInButton.isEnabled = inputEmail.text.isNotBlank() && inputPassword.text.isNotBlank()
-
+                    && checkboxTermsOfUse.isChecked
+            textView.text = ""
         }
 
-        logInButton.isEnabled =
-            inputEmail.text.isNotBlank() && inputPassword.text.isNotBlank() && checkboxTermsOfUse.isChecked
+        logInButton.setOnClickListener {
+            userRegistration()
+        }
 
+    }
+
+    private fun userRegistration(){
+            inputPassword.isEnabled = false
+            inputEmail.isEnabled = false
+            checkboxTermsOfUse.isEnabled = false
+            progressBarTerms.visibility = View.VISIBLE
+            logInButton.isEnabled = false
+
+
+            Handler().postDelayed({
+                inputPassword.isEnabled = true
+                inputPassword.setText("")
+                inputEmail.isEnabled = true
+                inputEmail.setText("")
+                checkboxTermsOfUse.isEnabled = true
+                checkboxTermsOfUse.isChecked = false
+                progressBarTerms.visibility = View.GONE
+                textView.text = "Регистрация прошла успешно"
+            }, 4000)
     }
 
 }
