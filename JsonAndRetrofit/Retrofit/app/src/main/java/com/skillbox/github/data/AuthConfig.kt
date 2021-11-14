bc8fb6881a3b4bad9bbe8288ebf1ac25af5caf85
VtShabanoff <vitalyshabanoff@yandex.ru>
2021-11-14 18:00:55 +0300

    registering an application

--- JsonAndRetrofit/Retrofit/app/src/main/java/com/skillbox/github/data/AuthConfig.kt
+++ JsonAndRetrofit/Retrofit/app/src/main/java/com/skillbox/github/data/AuthConfig.kt
@@ -9,7 +9,7 @@ object AuthConfig {
     const val RESPONSE_TYPE = ResponseTypeValues.CODE
     const val SCOPE = "user,repo"
 
-    const val CLIENT_ID = "YOUR CLIENT ID"
-    const val CLIENT_SECRET = "YOUR CLIENT SECRET"
+    const val CLIENT_ID = "d36346da1b998188354e"
+    const val CLIENT_SECRET = "afcd49e1be9ed50375a3a99f4557141155cf7c01"
     const val CALLBACK_URL = "skillbox://skillbox.ru/callback"
 }
